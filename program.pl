a1ea000d3c6e38647d922947191c16b747363179
Sergey Gorbunov <svgorbunov83@yandex.ru>
2021-02-14 08:14:51 +0300

    Add, sub expression.

--- program.pl
+++ program.pl
@@ -6,29 +6,77 @@ use warnings;
 my $look;
 
 &init();
+&expression();
 
 sub init {
 	&get_char();
 }
 
+## Lexer
+
+# Следующий символ
 sub get_char {
-	chomp ($look = <STDIN>);
+	$look = getc STDIN;
 }
 
-# (msg: string) -> void
-sub error {
-	print "[ERROR] $_[0].\n";
+# Идентификатор
+sub get_name {
+	if (!&is_alpha($look)) {
+		&expected("Name");
+	}
+	
+	my $result = uc $look;
+	&get_char();
+	$result;
 }
 
-sub abort {
-	&error($_[0]);
-	exit 1;
+# Число
+sub get_num {
+	if (!&is_digit($look)) {
+		&expected("Integer");
+	}
+	
+	my $result = uc $look;
+	&get_char();
+	$result;
 }
 
-sub expected {
-	&abort("$_[0] Expected");
+## Правила грамматики
+
+sub term {
+	&emitln("MOVE #" . &get_num() . ",D0");
 }
 
+sub expression {
+	&term();
+	while ($look =~ /[+-]/) {
+		&emitln("MOVE D0,-(SP)");
+		if ($look eq "+") {
+			&add(); 
+		} elsif ($look eq "-") {
+			&subtract();
+		} else {
+			&expected("Addop");
+		}			
+	}
+}
+
+sub add {
+	&match("+");
+	&term();
+	&emitln("ADD (SP)+,D0");
+}
+
+sub subtract {
+	&match("-");
+	&term();
+	&emitln("SUB (SP)+,D0");
+	&emitln("NEG D0");
+}
+
+## private
+
+# Проверка обязательного символа
 sub match {
 	my $x = $_[0];
 	if ($look eq $x) {
@@ -38,24 +86,15 @@ sub match {
 	}
 }
 
-sub isAlpha {
-	return $_[0] =~ m/[A-Z]/i;
+sub is_alpha {
+	$_[0] =~ m/[A-Z]/i;
 }
 
-sub isDigit {
-	return $_[0] =~ /[0-9]/i;
+sub is_digit {
+	$_[0] =~ /[0-9]/i;
 }
 
-sub get_name {
-	if (!&isAlpha($look)) {
-		&expected("Name");
-	}
-	
-	my $result = uc $look;
-	&get_char();
-	$result;
-}
-
+#  Возврат сформированной команды
 sub emit {
 	print "\t $_[0]";
 }
@@ -63,3 +102,20 @@ sub emit {
 sub emitln {
 	print "\t $_[0]\n";
 }
+
+## Обработка ошибок
+
+sub expected {
+	&abort("$_[0] Expected");
+}
+
+sub abort {
+	&error($_[0]);
+	exit 1;
+}
+
+sub error {
+	print "\n[ERROR] $_[0].\n";
+}
+
+1;
